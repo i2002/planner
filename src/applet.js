dd70f799ef522774d36b5241e5bfa821f499f37d
Tudor Butufei <tudor.butufei@gmail.com>
2019-02-07 10:35:43 +0200

    prevent hidding applet

--- src/applet.js
+++ src/applet.js
@@ -6,7 +6,7 @@ class Applet {
 
     newApplet() {
         if(aplicatie.context == "nw") {
-            nw.Window.open(`applet.html`, {width: 120, height: 120, always_on_top: true, frame: false, transparent: true, show_in_taskbar: false, show: false}, window => {
+            nw.Window.open(`applet.html`, {width: 120, height: 120, always_on_top: true, frame: false, visible_on_all_workspaces: true, transparent: true, show_in_taskbar: false, show: false}, window => {
                 this.window = window;
 
                 window.on("loaded", () => window.eval(null, `let appletfront = new AppletFront(${this.time})`));
@@ -14,6 +14,18 @@ class Applet {
                 window.window.addEventListener("details", () => {
                     this.showDetails();
                 })
+
+                //TODO: add settings to disable auto showing up
+                window.on("minimize", () => {
+                    window.restore();
+                })
+
+                window.on("closed", () => {
+                    this.window = null;
+                    if(this.time > 0) {
+                        this.newApplet();
+                    }
+                });
             })
         }
     }
